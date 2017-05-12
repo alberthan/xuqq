08f2da113177253290c4a44cb43d25933257d5ba
Albert Han <albert.han@autodesk.com>
2017-05-12 16:55:25 +0800

    Update 9gg.py

--- 9gg.py
+++ 9gg.py
@@ -15,8 +15,9 @@ def change(num):
         print 'num is odd number'
 
     elif num % 2 == 0:
-        print 'num is even number, exit()'
-        return 0
+        print 'num is even number, goto another function'
+        change_even(num)
+        exit(0)
         
     mid_number = num/2 
     print 'mid_number is : ',mid_number
@@ -26,15 +27,14 @@ def change(num):
             colomn_pos = mid_number
             print 'current num to be filled is: ', item
             print 'before fill 1, list is: ', list
-            #list[0][mid_number] = 1
+            
             list[0][mid_number] = 1
             print 'After fill 1, list is :', list
             print '1 has been filled , now num to be filled is: ', item
             item = item + 1
             
             row_pos = row_pos - 1
-            colomn_pos = colomn_pos + 1      
-  
+            colomn_pos = colomn_pos + 1
             
         elif item > 1 and item < num*num+1:
             print 'current item is: ', item
@@ -67,9 +67,33 @@ def change(num):
             print 'next item is: ', item
             print '*****************'
             continue
-
+            
     print 'list is: \n', list
 
+#以下num是几维数组的维度，偶数阶的处理方法    
+def change_even(num):
+    num = int(num)
+    sum = num*num + 1
+    #初始化list，告诉程序这是一个二维数组，防止不初始化直接填充的话会报错
+    list = [[0 for i in range(num)] for i in range(num)] 
+    
+    #首先把数字按顺序填进去
+    for i in range(0,num):
+        for j in range(0,num):
+            item = i * num + j + 1
+            list[i][j] = item
+    print 'before sorting, list is : ', list
+    
+    #开始交换顺序
+    for i in range(0,num):
+        for j in range(0,num):
+            if i == j :
+                list[i][j] = sum - list[i][j]
+            elif i + j == num -1:
+                list[i][j] = sum - list[i][j]
+  
+    print '\nafter sorting, list is : ', list
+    
 if __name__ == '__main__':
     change(raw_input('please input the number of rows for jiugongge: '))            
                     
